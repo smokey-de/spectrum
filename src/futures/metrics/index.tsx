3bb4a4b4528bd68713a081e38faaad246bd01491
UzWebDeveloper <max220700@gmail.com>
2023-08-10 23:46:10 +0500

    add changes

---
+++ src/futures/metrics/index.tsx
@@ -0,0 +1,13 @@
+import {useMetricsStore} from "../../shared/store/metrics";
+import {useEffect} from "react";
+import {shallow} from "zustand/shallow";
+
+export default function Metrics(){
+
+    return (
+        <>
+            kjkjkj
+
+        </>
+    )
+}
